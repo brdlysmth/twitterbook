72204dce6f538945d9b0821890b1314dd698a16b
brdlysmth <brsmith110@gmail.com>
2020-09-13 15:34:17 -0400

    can get text field from twitter api

---
+++ src/twitter/twitter.ts
@@ -0,0 +1,43 @@
+import axios, { AxiosRequestConfig } from 'axios';
+import fs from 'fs';
+import Twitter from 'twitter';
+
+const baseUrlV1 = 'https://api.twitter.com/1.1/';
+// const baseUrlV2 = 'https://api.twitter.com/2/';
+const username = 'acebucko';
+const count = 100;
+const requestUrl = `statuses/user_timeline.json?screen_name=${username}&count=${count}`;
+
+const config: AxiosRequestConfig = {
+  method: 'GET',
+  url: baseUrlV1 + requestUrl,
+  headers: {
+    Authorization:
+      'Bearer AAAAAAAAAAAAAAAAAAAAADOqHgEAAAAAVFkfrWt%2BZCL7qDK369fxPMfgPww%3DJdlBSw9bZatJ3gGFJyo4iai8WCIzsPSsk4JE6vxA2UdARsgR8j',
+    Cookie:
+      'personalization_id="v1_TSJfzR2GfyxXgMuHoBV+Qw=="; guest_id=v1%3A160002132457731207'
+  }
+};
+
+axios(config)
+  .then(function (response) {
+    // console.log(JSON.stringify(response.data));
+    const data = response.data;
+    filterTwitterResponse(data);
+    fs.writeFile('twitter-res.json', JSON.stringify(data), (error) => {
+      console.log(error);
+    });
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+const filterTwitterResponse = (data: Twitter.ResponseData) => {
+  const filteredData = data.map((tweet: any) => {
+    return {
+      tweet: tweet.text
+    };
+  });
+
+  console.log(filteredData);
+};
