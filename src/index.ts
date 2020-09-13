d0e1e412d2866a747852e55b31debf020975798d
brdlysmth <brsmith110@gmail.com>
2020-09-13 18:22:29 -0400

    adding html entry

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+const PORT = 1000;
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  console.log(__dirname);
+  res.sendFile('public/index.html', { root: __dirname });
+  //   res.send('hello world');
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
