bfce3744babcf76a22ffa28594ade4ad760c0581
brdlysmth <brsmith110@gmail.com>
2020-09-13 18:25:17 -0400

    editing html input, need to create new app with twitter backend

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const PORT = 1000;
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   console.log(__dirname);
-  res.sendFile('public/index.html', { root: __dirname });
+  res.sendFile('src/public/index.html', { root: __dirname });
   //   res.send('hello world');
 });
 
