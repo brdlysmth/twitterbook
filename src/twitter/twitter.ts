c830a454176d0f512590325c23e9fc20b485d04f
brdlysmth <brsmith110@gmail.com>
2020-09-14 00:29:46 -0400

    filtering by likes

--- src/twitter/twitter.ts
+++ src/twitter/twitter.ts
@@ -1,6 +1,9 @@
 import axios, { AxiosRequestConfig } from 'axios';
+import dotenv from 'dotenv';
 import fs from 'fs';
 import Twitter from 'twitter';
+
+dotenv.config();
 
 const baseUrlV1 = 'https://api.twitter.com/1.1/';
 // const baseUrlV2 = 'https://api.twitter.com/2/';
@@ -12,7 +15,7 @@ const config: AxiosRequestConfig = {
   method: 'GET',
   url: baseUrlV1 + requestUrl,
   headers: {
-    Authorization: 'Bearer ' + process.env.TWITTER_BEAERER_TOKEN,
+    Authorization: 'Bearer ' + process.env.TWITTER_BEARER_TOKEN,
     Cookie:
       'personalization_id="v1_TSJfzR2GfyxXgMuHoBV+Qw=="; guest_id=v1%3A160002132457731207'
   }
@@ -31,11 +34,19 @@ axios(config)
 const filterTwitterResponse = (data: Twitter.ResponseData) => {
   const filteredData = data.map((tweet: any) => {
     return {
-      tweet: tweet.text
+      tweet: tweet.text,
+      date: tweet.created_at,
+      favorites: tweet.favorite_count
     };
   });
 
-  fs.writeFile('first.json', JSON.stringify(filteredData), (error) => {
+  const newFilteredData = filteredData.filter((tweet: any) => {
+    if (tweet.favorites > 10000) {
+      return tweet;
+    }
+  });
+
+  fs.writeFile('first.json', JSON.stringify(newFilteredData), (error) => {
     console.log(error);
   });
 
